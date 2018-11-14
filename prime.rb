76f74d22cc40760a5b373d7f690fd06553eaa2a4
fortunate-database-4943 <chris@chrisphilbin.net>
2018-11-14 01:20:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,4 @@
 def prime?(i)
    prime = (2..(i - 1)).to_a
-  prime.any? { |d| i % d} == 0 || i <= 0 || i == 1 ? false : true
+  prime.any? { |d| i % d == 0} || i <= 0 || i == 1 ? false : true
 end
